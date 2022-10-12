2acae75181e9b5cd903ce129af3e8567f0fe8e4f
William Polinchak <polinchw@gmail.com>
2022-10-12 16:28:17 -0400

    Initial commit

---
+++ modules/cluster/variables.tf
@@ -0,0 +1,16 @@
+variable "serviceprinciple_id" {
+}
+
+variable "serviceprinciple_key" {
+}
+
+variable "location" {
+  default = "eastus"
+}
+
+variable "kubernetes_version" {
+    default = "1.23.8"
+}
+
+variable "ssh_key" {
+}
