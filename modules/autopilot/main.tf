10dc256eaa058594a810e9d5bf1e50ab1a4b634e
William Polinchak <william.polinchak@windstream.com>
2022-10-14 15:05:25 -0400

    Ready to test.

--- modules/autopilot/main.tf
+++ modules/autopilot/main.tf
@@ -6,7 +6,7 @@ provider "kubectl" {
 resource "null_resource" "install_autopilot" {
 
   provisioner "local-exec" {
-    command     = "./modules/autopilot/install_autopilot.sh ${git_token} ${git_repo}"
+    command     = "./modules/autopilot/install_autopilot.sh ${var.git_token} ${var.git_repo}"
     interpreter = ["/bin/bash", "-c"]
   }
 }
