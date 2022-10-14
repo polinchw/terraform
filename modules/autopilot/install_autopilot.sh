eabc0b2501996fc834ee360d93cb9c012ff2f992
William Polinchak <william.polinchak@windstream.com>
2022-10-14 14:31:19 -0400

    Adding autopilot download.

--- modules/autopilot/install_autopilot.sh
+++ modules/autopilot/install_autopilot.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-echo "hello world"
+echo "Installing autopilot..."
+# get the latest version or change to a specific version
+VERSION=$(curl --silent "https://api.github.com/repos/argoproj-labs/argocd-autopilot/releases/latest" | grep '"tag_name"' | sed -E 's/.*"([^"]+)".*/\1/')
+
+# download and extract the binary
+curl -L --output - https://github.com/argoproj-labs/argocd-autopilot/releases/download/$VERSION/argocd-autopilot-darwin-amd64.tar.gz | tar zx
+
+
+# check the installation
+# ./argocd-autopilot- version
